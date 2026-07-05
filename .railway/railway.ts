cad4533b584aab5b5c133f1349437eeeaf46f2f5
Bashir <20244544+bmja62@users.noreply.github.com>
2026-07-05 08:50:20 +0000

    refactor: update Railway configuration for services and improve environment handling

--- .railway/railway.ts
+++ .railway/railway.ts
@@ -4,12 +4,11 @@ export default defineRailway((ctx) => {
   const prod = ctx.environment === "production";
   const branch = "main";
 
- const PostgresWTEN = postgres("Postgres-WTEN");
+  
+  const PostgresWTEN = postgres("Postgres-WTEN");
   const postgresVolume = volume("postgres-volume", { alerts: { usage: { "100": {}, "80": {}, "95": {} } }, allowOnlineResize: true, region: "sfo", sizeMB: 500 });
   const postgresVolumeJ38W = volume("postgres-volume-j38W", { alerts: { usage: { "100": {}, "80": {}, "95": {} } }, allowOnlineResize: true, region: "sfo", sizeMB: 5000 });
  
-
-
   
   const api = service("api", {
     source: github("bmja62/padylife", { branch, rootDirectory: "api" }),
@@ -21,37 +20,45 @@ export default defineRailway((ctx) => {
     healthcheckTimeout: 300,
     replicas: 1,
     env: {
-      ASPNETCORE_ENVIRONMENT: ctx.environment,
+      ASPNETCORE_ENVIRONMENT: prod ? "Production" : "Staging",
+      aspnetcore_url: prod ? "https://api.padylife.ir" : "https://staging-api.padylife.ir",
       ConnectionStrings__PostgreSQL: preserve(),
     },
   });
 
   const app = service("app", {
     source: github("bmja62/padylife", { branch, rootDirectory: "app" }),
-    build: "pnpm run generate",
-    start: "npx --yes serve@14 -s .output/public -l $PORT",
+    build:{
+      builder: "DOCKERFILE",
+      dockerfilePath: "app/Dockerfile",
+    },
     replicas: 1,
     env: {
-      NUXT_PUBLIC_API_ADDRESS: prod ? "https://api.padylife.ir" : `https://${api.env.RAILWAY_PUBLIC_DOMAIN}`,
+      NUXT_PUBLIC_API_ADDRESS: prod ? "https://app.padylife.ir" : "https://staging-app.padylife.ir",
     },
   });
   const admin = service("admin", {
     source: github("bmja62/padylife", { branch, rootDirectory: "admin" }),
-    build: "yarn build",
-    start: "npx --yes serve@14 -s dist -l $PORT",
+    build:{
+      builder: "DOCKERFILE",
+      dockerfilePath: "admin/Dockerfile",
+    },
     replicas: 1,
     env: {
-      VITE_BASE_API_URL: prod ? "https://api.padylife.ir" : `https://${api.env.RAILWAY_PUBLIC_DOMAIN}`,
+      VITE_BASE_API_URL: prod ? "https://admin.padylife.ir" : "https://staging-admin.padylife.ir",
     },
   });
 
   const www = service("www", {
-    source: github("bmja62/padylife", { branch, rootDirectory: "." }),
+    source: github("bmja62/padylife", { branch, rootDirectory: "www" }),
     build: {
       builder: "DOCKERFILE",
       dockerfilePath: "www/Dockerfile",
     },
     replicas: 1,
+    env: {
+      NUXT_PUBLIC_API_ADDRESS: prod ? "https://www.padylife.ir" : "https://staging-www.padylife.ir",
+    },
   });
 
   return project("padylife", {
